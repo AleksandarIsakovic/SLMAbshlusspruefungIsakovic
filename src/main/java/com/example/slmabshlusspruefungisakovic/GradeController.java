73c8b1eefbce12230b96302ce5a668c5fd54fc2b
AleksandarIsakovic <ic22b074@technikum-wien.at>
2022-12-21 18:49:54 +0100

    added logic to controller class

--- src/main/java/com/example/slmabshlusspruefungisakovic/GradeController.java
+++ src/main/java/com/example/slmabshlusspruefungisakovic/GradeController.java
@@ -1,7 +1,28 @@
 package com.example.slmabshlusspruefungisakovic;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GradeController {
+
+    @GetMapping("/grade")
+    public int getGrade(@RequestParam int percentage){
+       if(percentage >=88){
+           return 1;
+       } else if (percentage >= 75) {
+           return 2;
+       } else if (percentage >= 63) {
+           return 3;
+       } else if (percentage>= 50) {
+           return 4;
+       }else{
+       return 5;}
+
+
+    }
+
+
+
 }
