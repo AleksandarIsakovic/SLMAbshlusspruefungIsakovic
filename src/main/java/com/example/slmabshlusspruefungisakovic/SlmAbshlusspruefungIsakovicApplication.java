a5bb5c63fa034c56090f4ed0b7b47386ae2707ad
AleksandarIsakovic <ic22b074@technikum-wien.at>
2022-12-21 18:13:26 +0100

    Initial Commit

---
+++ src/main/java/com/example/slmabshlusspruefungisakovic/SlmAbshlusspruefungIsakovicApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmabshlusspruefungisakovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmAbshlusspruefungIsakovicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmAbshlusspruefungIsakovicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmabshlusspruefungisakovic/SlmAbshlusspruefungIsakovicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmabshlusspruefungisakovic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmAbshlusspruefungIsakovicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
